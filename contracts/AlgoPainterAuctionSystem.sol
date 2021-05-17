aced5404f629a7ed187b044aea398937d4fe6886
Gleisson de Assis <gleisson.assis@gmail.com>
2021-05-17 12:33:31 -0300

    First version of AuctionSystem

---
+++ contracts/AlgoPainterAuctionSystem.sol
@@ -0,0 +1,443 @@
+// SPDX-License-Identifier: GPL-v3
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+import "./AlgoPainterAuctionSystemAccessControl.sol";
+
+contract AlgoPainterAuctionSystem is
+    AlgoPainterAuctionSystemAccessControl,
+    ERC1155Holder,
+    ERC721Holder
+{
+    using SafeMath for uint256;
+
+    uint256 private constant ONE_HUNDRED_PERCENT = 10**4;
+    bytes private DEFAULT_MESSAGE;
+
+    mapping(uint256 => mapping(address => uint256)) private pendingReturns;
+
+    enum TokenType {ERC721, ERC1155}
+
+    struct AuctionInfo {
+        address beneficiary;
+        TokenType tokenType;
+        address tokenAddress;
+        uint256 tokenId;
+        uint256 minimumAmount;
+        uint256 auctionEndTime;
+        IERC20 tokenPriceAddress;
+        uint256 bidBackFee;
+        bool ended;
+        address highestBidder;
+        uint256 highestBid;
+    }
+
+    AuctionInfo[] private auctionInfo;
+    mapping(address => mapping(uint256 => uint256)) private auctions;
+
+    IERC20[] private allowedTokens;
+    mapping(IERC20 => bool) private allowedTokensMapping;
+
+    event HighestBidIncreased(
+        uint256 indexed auctionId,
+        address bidder,
+        uint256 amount,
+        uint256 feeAmount,
+        uint256 netAmount
+    );
+
+    event AuctionEnded(
+        uint256 auctionId,
+        address winner,
+        uint256 amount,
+        uint256 feeAmount,
+        uint256 netAmount
+    );
+
+    address addressFee;
+    uint256 auctionFeeRate;
+    uint256 bidFeeRate;
+
+    event AuctionSystemSetup(
+        address addressFee,
+        uint256 auctionFeeRate,
+        uint256 bidFeeRate,
+        IERC20[] allowedTokens
+    );
+
+    function getNow() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function getAddressFee() public view returns (address) {
+        return addressFee;
+    }
+
+    function getAuctionFeeRate() public view returns (uint256) {
+        return auctionFeeRate;
+    }
+
+    function getBidFeeRate() public view returns (uint256) {
+        return bidFeeRate;
+    }
+
+    function getAllowedTokens() public view returns (IERC20[] memory) {
+        return allowedTokens;
+    }
+
+    function setup(
+        address _addressFee,
+        uint256 _auctionFeeRate,
+        uint256 _bidFeeRate,
+        IERC20[] memory _allowedTokens
+    ) public onlyRole(CONFIGURATOR_ROLE) {
+        require(
+            _auctionFeeRate <= ONE_HUNDRED_PERCENT,
+            "AlgoPainterAuctionSystem:INVALID_AUCTION_FEE"
+        );
+        require(
+            _bidFeeRate <= ONE_HUNDRED_PERCENT,
+            "AlgoPainterAuctionSystem:INVALID_BID_FEE"
+        );
+
+        addressFee = _addressFee;
+        auctionFeeRate = _auctionFeeRate;
+        bidFeeRate = _bidFeeRate;
+
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            allowedTokensMapping[allowedTokens[i]] = false;
+        }
+
+        allowedTokens = _allowedTokens;
+
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            allowedTokensMapping[allowedTokens[i]] = true;
+        }
+
+        emit AuctionSystemSetup(
+            _addressFee,
+            _auctionFeeRate,
+            _bidFeeRate,
+            _allowedTokens
+        );
+    }
+
+    function setFeeAddress(address _addressFee)
+        public
+        onlyRole(CONFIGURATOR_ROLE)
+    {
+        addressFee = _addressFee;
+
+        emit AuctionSystemSetup(
+            addressFee,
+            auctionFeeRate,
+            bidFeeRate,
+            allowedTokens
+        );
+    }
+
+    function setupFees(uint256 _auctionFeeRate, uint256 _bidFeeRate)
+        public
+        onlyRole(CONFIGURATOR_ROLE)
+    {
+        require(
+            _auctionFeeRate <= ONE_HUNDRED_PERCENT,
+            "AlgoPainterAuctionSystem:INVALID_AUCTION_FEE"
+        );
+        require(
+            _bidFeeRate <= ONE_HUNDRED_PERCENT,
+            "AlgoPainterAuctionSystem:INVALID_BID_FEE"
+        );
+
+        auctionFeeRate = _auctionFeeRate;
+        bidFeeRate = _bidFeeRate;
+
+        emit AuctionSystemSetup(
+            addressFee,
+            auctionFeeRate,
+            bidFeeRate,
+            allowedTokens
+        );
+    }
+
+    function addAllowedToken(IERC20 _allowedToken)
+        public
+        onlyRole(CONFIGURATOR_ROLE)
+    {
+        allowedTokens.push(_allowedToken);
+        allowedTokensMapping[_allowedToken] = true;
+
+        emit AuctionSystemSetup(
+            addressFee,
+            auctionFeeRate,
+            bidFeeRate,
+            allowedTokens
+        );
+    }
+
+    function createAuction(
+        TokenType _tokenType,
+        address _tokenAddress,
+        uint256 _tokenId,
+        uint256 _minimumAmount,
+        uint256 _auctionEndTime,
+        IERC20 _tokenPriceAddress,
+        uint256 _bidBackFee
+    ) public returns (uint256) {
+        if (_tokenType == TokenType.ERC721) {
+            IERC721 token = IERC721(_tokenAddress);
+            require(
+                token.isApprovedForAll(msg.sender, address(this)),
+                "AlgoPainterAuctionSystem:ERC721_NOT_APPROVED"
+            );
+
+            token.safeTransferFrom(msg.sender, address(this), _tokenId);
+        } else {
+            IERC1155 token = IERC1155(_tokenAddress);
+            require(
+                token.isApprovedForAll(msg.sender, address(this)),
+                "AlgoPainterAuctionSystem:ERC1155_NOT_APPROVED"
+            );
+
+            token.safeTransferFrom(
+                msg.sender,
+                address(this),
+                _tokenId,
+                1,
+                DEFAULT_MESSAGE
+            );
+        }
+
+        require(
+            _auctionEndTime > getNow(),
+            "AlgoPainterAuctionSystem:INVALID_TIME_STAMP"
+        );
+
+        require(
+            allowedTokensMapping[_tokenPriceAddress],
+            "AlgoPainterAuctionSystem:INVALID_TOKEN_PRICE_ADDRESS"
+        );
+
+        require(
+            _bidBackFee <= ONE_HUNDRED_PERCENT,
+            "AlgoPainterAuctionSystem:INVALID_BID_BACK_PERCENTUAL"
+        );
+
+        auctionInfo.push(
+            AuctionInfo(
+                msg.sender,
+                _tokenType,
+                _tokenAddress,
+                _tokenId,
+                _minimumAmount,
+                _auctionEndTime,
+                _tokenPriceAddress,
+                _bidBackFee,
+                false,
+                address(0),
+                0
+            )
+        );
+
+        auctions[_tokenAddress][_tokenId] = auctionInfo.length.sub(1);
+
+        return auctions[_tokenAddress][_tokenId];
+    }
+
+    function getAuctionLength() public view returns (uint256) {
+        return auctionInfo.length;
+    }
+
+    function getAuctionId(address _tokenAddress, uint256 _tokenId)
+        public
+        view
+        returns (uint256)
+    {
+        return auctions[_tokenAddress][_tokenId];
+    }
+
+    function getAuctionInfo(uint256 _auctionId)
+        public
+        view
+        returns (
+            address beneficiary,
+            TokenType tokenType,
+            address tokenAddress,
+            uint256 tokenId,
+            uint256 minimumAmount,
+            uint256 auctionEndTime,
+            IERC20 tokenPriceAddress,
+            uint256 bidBackFee,
+            bool ended,
+            address highestBidder,
+            uint256 highestBid
+        )
+    {
+        AuctionInfo storage auctionInfo = auctionInfo[_auctionId];
+
+        beneficiary = auctionInfo.beneficiary;
+        tokenType = auctionInfo.tokenType;
+        tokenAddress = auctionInfo.tokenAddress;
+        tokenId = auctionInfo.tokenId;
+        minimumAmount = auctionInfo.minimumAmount;
+        auctionEndTime = auctionInfo.auctionEndTime;
+        tokenPriceAddress = auctionInfo.tokenPriceAddress;
+        bidBackFee = auctionInfo.bidBackFee;
+        ended = auctionInfo.ended;
+        highestBidder = auctionInfo.highestBidder;
+        highestBid = auctionInfo.highestBid;
+    }
+
+    function bid(uint256 _auctionId, uint256 _amount) public {
+        AuctionInfo storage auctionInfo = auctionInfo[_auctionId];
+
+        require(
+            getNow() <= auctionInfo.auctionEndTime,
+            "AlgoPainterAuctionSystem:AUCTION_ENDED"
+        );
+
+        require(
+            _amount > auctionInfo.highestBid,
+            "AlgoPainterAuctionSystem:LOW_BID"
+        );
+
+        require(
+            _amount >= auctionInfo.minimumAmount,
+            "AlgoPainterAuctionSystem:LOW_BID_MINIMUM_AMOUNT"
+        );
+
+        IERC20 tokenPrice = IERC20(auctionInfo.tokenPriceAddress);
+
+        (uint256 netAmount, uint256 feeAmount) = getBidAmountInfo(_amount);
+
+        require(
+            tokenPrice.transferFrom(msg.sender, address(this), _amount),
+            "AlgoPainterAuctionSystem:FAIL_TO_TRANSFER_BID_AMOUNT"
+        );
+
+        require(
+            tokenPrice.transfer(addressFee, feeAmount),
+            "AlgoPainterAuctionSystem:FAIL_TO_TRANSFER_FEE_AMOUNT"
+        );
+
+        if (auctionInfo.highestBid != 0) {
+            pendingReturns[_auctionId][
+                auctionInfo.highestBidder
+            ] = pendingReturns[_auctionId][auctionInfo.highestBidder].add(
+                auctionInfo.highestBid
+            );
+        }
+
+        auctionInfo.highestBidder = msg.sender;
+        auctionInfo.highestBid = netAmount;
+
+        emit HighestBidIncreased(
+            _auctionId,
+            msg.sender,
+            _amount,
+            feeAmount,
+            netAmount
+        );
+    }
+
+    function getClaimableAmount(uint256 _auctionId, address _address)
+        public
+        view
+        returns (uint256)
+    {
+        pendingReturns[_auctionId][_address];
+    }
+
+    function withdraw(uint256 _auctionId) public {
+        AuctionInfo storage auctionInfo = auctionInfo[_auctionId];
+        uint256 amount = pendingReturns[_auctionId][msg.sender];
+
+        if (amount > 0) {
+            pendingReturns[_auctionId][msg.sender] = 0;
+            IERC20 tokenPrice = IERC20(auctionInfo.tokenPriceAddress);
+
+            require(
+                tokenPrice.transfer(msg.sender, amount),
+                "AlgoPainterAuctionSystem:FAIL_TO_WITHDRAW"
+            );
+        } else {
+            revert("AlgoPainterAuctionSystem:NOTHING_TO_WITHDRAW");
+        }
+    }
+
+    function getFeeAndNetAmount(uint256 _amount, uint256 fee)
+        public
+        pure
+        returns (uint256 netAmount, uint256 feeAmount)
+    {
+        feeAmount = _amount.mul(fee).div(ONE_HUNDRED_PERCENT);
+        netAmount = _amount.sub(feeAmount);
+    }
+
+    function getAuctionAmountInfo(uint256 _amount)
+        public
+        view
+        returns (uint256 netAmount, uint256 feeAmount)
+    {
+        (netAmount, feeAmount) = getFeeAndNetAmount(_amount, auctionFeeRate);
+    }
+
+    function getBidAmountInfo(uint256 _amount)
+        public
+        view
+        returns (uint256 netAmount, uint256 feeAmount)
+    {
+        (netAmount, feeAmount) = getFeeAndNetAmount(_amount, bidFeeRate);
+    }
+
+    function endAction(uint256 _auctionId) public {
+        AuctionInfo storage auctionInfo = auctionInfo[_auctionId];
+        IERC20 tokenPrice = IERC20(auctionInfo.tokenPriceAddress);
+
+        require(
+            getNow() >= auctionInfo.auctionEndTime,
+            "AlgoPainterAuctionSystem:NOT_YET_ENDED"
+        );
+        require(!auctionInfo.ended, "AlgoPainterAuctionSystem:ALREADY_ENDED");
+
+        address winner = auctionInfo.highestBidder;
+        uint256 bidAmount = auctionInfo.highestBid;
+        (uint256 feeAmount, uint256 netAmount) =
+            getAuctionAmountInfo(bidAmount);
+
+        require(
+            tokenPrice.transfer(auctionInfo.beneficiary, netAmount),
+            "AlgoPainterAuctionSystem:FAIL_TO_PAY_BENEFICIARY"
+        );
+
+        require(
+            tokenPrice.transfer(addressFee, feeAmount),
+            "AlgoPainterAuctionSystem:FAIL_TO_PAY_DEVADDRESS"
+        );
+
+        if (auctionInfo.tokenType == TokenType.ERC721) {
+            IERC721 token = IERC721(auctionInfo.tokenAddress);
+            token.safeTransferFrom(address(this), winner, auctionInfo.tokenId);
+        } else {
+            IERC1155 token = IERC1155(auctionInfo.tokenAddress);
+            token.safeTransferFrom(
+                address(this),
+                winner,
+                auctionInfo.tokenId,
+                1,
+                DEFAULT_MESSAGE
+            );
+        }
+
+        auctionInfo.ended = true;
+
+        emit AuctionEnded(_auctionId, winner, bidAmount, feeAmount, netAmount);
+    }
+}
---
+++ test/AlgoPainterAuctionSystem.test.js
@@ -0,0 +1,185 @@
+const AlgoPainterToken = artifacts.require('AlgoPainterToken');
+const AlgoPainterGweiItem = artifacts.require('AlgoPainterGweiItem');
+const AlgoPainterAuctionSystem = artifacts.require('AlgoPainterAuctionSystem');
+var sleep = require('sleep');
+
+contract('AlgoPainterAuctionSystem', accounts => {
+  let algop = null;
+  let busd = null;
+  let eth = null;
+  let gwei = null;
+  let auction = null;
+
+  it.only('should deploy the contracts', async () => {
+    algop = await AlgoPainterToken.new("AlgoPainter Token", "ALGOP");
+    busd = await AlgoPainterToken.new("BUSD", "BUSD");
+    eth = await AlgoPainterToken.new("ETH", "ETH");
+
+    gwei = await AlgoPainterGweiItem.new(algop.address, accounts[8]);
+
+    auction = await AlgoPainterAuctionSystem.new();
+
+    const amount = web3.utils.toWei('300', 'ether');
+
+    await algop.approve(gwei.address, amount);
+    await gwei.mint(1, 'new text', false, 0, 2, amount, 'URI');
+  });
+
+  it.only('should setup auction system', async () => {
+    await auction.setup(accounts[9], 1000, 250, [algop.address, busd.address, eth.address]);
+
+    expect(await auction.getAddressFee()).to.be.equal(accounts[9]);
+    expect((await auction.getAuctionFeeRate()).toString()).to.be.equal('1000');
+    expect((await auction.getBidFeeRate()).toString()).to.be.equal('250');
+
+    const allowedTokens = await auction.getAllowedTokens();
+
+    expect(allowedTokens.length).to.be.equal(3);
+    expect(allowedTokens[0]).to.be.equal(algop.address);
+    expect(allowedTokens[1]).to.be.equal(busd.address);
+    expect(allowedTokens[2]).to.be.equal(eth.address);
+  });
+
+  it.only('should create an auction', async () => {
+    const now = parseInt((await auction.getNow()).toString());
+    const expirationTime = (now + 20).toString();
+
+    try {
+      await auction.createAuction(0, gwei.address, 1, web3.utils.toWei('100', 'ether'), expirationTime, algop.address, 10000);
+      throw {};
+    } catch (e) {
+      expect(e.reason).to.be.equal('AlgoPainterAuctionSystem:ERC721_NOT_APPROVED');
+    }
+
+    await gwei.setApprovalForAll(auction.address, true);
+    await auction.createAuction(0, gwei.address, 1, web3.utils.toWei('100', 'ether'), expirationTime, algop.address, 10000);
+
+    const auctionId = await auction.getAuctionId(gwei.address, 1);
+
+    const auctionInfo = await auction.getAuctionInfo(auctionId);
+
+    expect(auctionInfo.tokenType.toString()).to.be.equal('0', 'fail to check tokenType');
+    expect(auctionInfo.tokenAddress).to.be.equal(gwei.address, 'fail to check tokenAddress');
+    expect(auctionInfo.tokenId.toString()).to.be.equal('1', 'fail to check tokenId');
+    expect(auctionInfo.minimumAmount.toString()).to.be.equal(web3.utils.toWei('100', 'ether'), 'fail to check minimumAmount');
+    expect(auctionInfo.auctionEndTime.toString()).to.be.equal(expirationTime, 'fail to check auctionEndTime');
+    expect(auctionInfo.tokenPriceAddress).to.be.equal(algop.address, 'fail to check tokenPriceAddress');
+    expect(auctionInfo.bidBackFee.toString()).to.be.equal('10000', 'fail to check bidBackFee');
+  });
+
+  it.only('should send bids', async () => {
+    const transferAmount = web3.utils.toWei('1000', 'ether');
+    await algop.transfer(accounts[1], transferAmount);
+    await algop.transfer(accounts[2], transferAmount);
+    await algop.transfer(accounts[3], transferAmount);
+
+    await algop.approve(auction.address, transferAmount, {from: accounts[1]});
+    await algop.approve(auction.address, transferAmount, {from: accounts[2]});
+    await algop.approve(auction.address, transferAmount, { from: accounts[3] });
+
+    const auctionId = await auction.getAuctionId(gwei.address, 1);
+    let feeAddressBalance = 0;
+    let auctionBalance = 0;
+
+    try
+    {
+      await auction.bid(auctionId, web3.utils.toWei('10', 'ether'), { from: accounts[1] });
+      throw {};
+    } catch (e) {
+      expect(e.reason).to.be.equal('AlgoPainterAuctionSystem:LOW_BID_MINIMUM_AMOUNT');
+    }
+
+    await auction.bid(auctionId, web3.utils.toWei('100', 'ether'), { from: accounts[1] });
+    feeAddressBalance = await algop.balanceOf(accounts[9]);
+    auctionBalance = await algop.balanceOf(auction.address);
+
+    expect(feeAddressBalance.toString()).to.be.equal('2500000000000000000', 'fail to check feeAddressBalance #1');
+    expect(auctionBalance.toString()).to.be.equal('97500000000000000000', 'fail to check auctionBalance #1');
+
+    try
+    {
+      await auction.bid(auctionId, web3.utils.toWei('90', 'ether'), { from: accounts[2] });
+      throw {};
+    } catch (e) {
+      expect(e.reason).to.be.equal('AlgoPainterAuctionSystem:LOW_BID');
+    }
+
+    await auction.bid(auctionId, web3.utils.toWei('101', 'ether'), { from: accounts[2] });
+    feeAddressBalance = await algop.balanceOf(accounts[9]);
+    auctionBalance = await algop.balanceOf(auction.address);
+
+    expect(feeAddressBalance.toString()).to.be.equal('5025000000000000000', 'fail to check feeAddressBalance #2');
+    expect(auctionBalance.toString()).to.be.equal('195975000000000000000', 'fail to check auctionBalance #2');
+
+    await auction.bid(auctionId, web3.utils.toWei('101.1', 'ether'), { from: accounts[3] });
+
+    const auctionInfo = await auction.getAuctionInfo(auctionId);
+
+    expect(auctionInfo.highestBidder).to.be.equal(accounts[3], 'fail to check highestBidder');
+    expect(auctionInfo.highestBid.toString()).to.be.equal(web3.utils.toWei('98.5725', 'ether'), 'fail to check highestBid');
+    feeAddressBalance = await algop.balanceOf(accounts[9]);
+    auctionBalance = await algop.balanceOf(auction.address);
+
+    expect(feeAddressBalance.toString()).to.be.equal('7552500000000000000', 'fail to check feeAddressBalance #3');
+    expect(auctionBalance.toString()).to.be.equal('294547500000000000000', 'fail to check auctionBalance #3');
+  });
+
+  it.only('should end an auction', async () => {
+
+    const auctionId = await auction.getAuctionId(gwei.address, 1);
+
+    try {
+      await auction.endAction(auctionId);
+      throw {};
+    } catch (e) {
+      expect(e.reason).to.be.equal('AlgoPainterAuctionSystem:NOT_YET_ENDED');
+    }
+
+    console.log('Waiting 20s to finish the auction');
+    sleep.sleep(20);
+    await auction.endAction(auctionId);
+
+    const feeAddressBalance = await algop.balanceOf(accounts[9]);
+    const auctionBalance = await algop.balanceOf(auction.address);
+
+    expect(feeAddressBalance.toString()).to.be.equal('96267750000000000000', 'fail to check feeAddressBalance');
+    expect(auctionBalance.toString()).to.be.equal('195975000000000000000', 'fail to check auctionBalance');
+
+    const auctionInfo = await auction.getAuctionInfo(auctionId);
+
+    const nftOwner = await gwei.ownerOf(1);
+
+    expect(nftOwner).to.be.equal(auctionInfo.highestBidder, 'fail to check nftOwner');
+  });
+
+  it.only('should withdraw remaining amounts', async () => {
+    const auctionId = await auction.getAuctionId(gwei.address, 1);
+
+    let account1Balance = await algop.balanceOf(accounts[1]);
+    let account2Balance = await algop.balanceOf(accounts[2]);
+    let account3Balance = await algop.balanceOf(accounts[3]);
+
+    expect(account1Balance.toString()).to.be.equal('900000000000000000000', 'fail to check account1Balance');
+    expect(account2Balance.toString()).to.be.equal('899000000000000000000', 'fail to check account2Balance');
+    expect(account3Balance.toString()).to.be.equal('898900000000000000000', 'fail to check account3Balance');
+
+    await auction.withdraw(auctionId, { from: accounts[1] });
+    await auction.withdraw(auctionId, { from: accounts[2] });
+
+    try
+    {
+      await auction.withdraw(auctionId, { from: accounts[3] });
+      throw {};
+    } catch (e) {
+      expect(e.reason).to.be.equal('AlgoPainterAuctionSystem:NOTHING_TO_WITHDRAW');
+    }
+
+    account1Balance = await algop.balanceOf(accounts[1]);
+    account2Balance = await algop.balanceOf(accounts[2]);
+    account3Balance = await algop.balanceOf(accounts[3]);
+
+    expect(account1Balance.toString()).to.be.equal('997500000000000000000', 'fail to check account1Balance');
+    expect(account2Balance.toString()).to.be.equal('997475000000000000000', 'fail to check account2Balance');
+    expect(account3Balance.toString()).to.be.equal('898900000000000000000', 'fail to check account3Balance');
+  });
+});
